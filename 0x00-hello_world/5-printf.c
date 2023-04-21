ec348c7fd590bc8cec20c5f34459248893c868f5
Es-say <eseobels@gmail.com>
2023-04-20 21:12:09 -0700

    edited

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
-int main(void)
+int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n";
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
 }
