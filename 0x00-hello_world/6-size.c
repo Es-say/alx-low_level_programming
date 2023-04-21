dcdabcb644c8ddb4306398aad3fc81a0252c7419
Es-say <eseobels@gmail.com>
2023-04-20 22:03:13 -0700

    size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
     float floatType;
 
     /*Sizeof operator is used to evaluate the size of a variable*/
-    printf("Size of a char:%ld byte(s)\n", sizeof(charType));
-    printf("Size of an int:%ld byte(s)\n", sizeof(integerType));
-    printf("Size of a long int:%ld byte(s)\n", sizeof(longintegerType));
-    printf("Size of a long long int:%ld byte(s)\n", sizeof(longlongintegerType));
-    printf("Size of float:%ld byte(s)\n", sizeof(floatType));
+ printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+ printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+ printf("Size of a long int: %ld byte(s)\n", sizeof(longintegerType));
+ printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintegerType));
+ printf("Size of float: %ld byte(s)\n", sizeof(floatType));
 
     return (0);
 }
