dd5935c34d0d87f7c38222d11c15f93b4c1fb1ac
Es-say <eseobels@gmail.com>
2023-04-25 19:52:06 -0700

    alphabet commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  * Return: Always return 0
  */
  
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
-        
-		_putchar(x);
-	
+        	_putchar(x);
 	
 	_putchar("\n");
 }
