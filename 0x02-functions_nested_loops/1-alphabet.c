6130eeb5116880e1c2735771a7def03b5f766d18
Es-say <eseobels@gmail.com>
2023-04-25 19:23:42 -0700

    alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet from a-z
+ *
+ * Return: Always return 0
+ */
+ 
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	for (x = 'a'; x <= 'z'; x++)
+        putchar(x);
+
+	return (0);
+
