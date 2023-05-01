d65be629d296be7797459b4eb0569b8f61b77e08
Es-say <eseobels@gmail.com>
2023-05-01 12:14:22 -0700

    prints alphabets except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
- * Description - prints the alphabets except e and q
- * return: always 0
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description; using the main function
+ * this program prints "alphabets except e and q"
+ * Return: 0
  */
 
 int main(void)
@@ -11,15 +13,16 @@ int main(void)
 	int n = 97;
 
 	while (n <= 122)
-{
+	{
 		if (n == 101 || n == 113)
 		{
 			n++;
 			continue;
 		}
-	putchar(n);
-	n++;
-}
+		putchar(n);
+		n++;
+	}
 	putchar('\n');
+
 	return (0);
 }
