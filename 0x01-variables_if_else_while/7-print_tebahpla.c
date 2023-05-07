9ab655f87f0a081f1627f5eab703775f2de16a7a
Es-say <eseobels@gmail.com>
2023-05-07 14:33:16 -0700

    prints the alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description - prints the lowercase alphabet in reverse
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+
+	putchar('\n');
+	return (0);
+}
