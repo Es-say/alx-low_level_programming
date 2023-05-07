a2786eefa324ff7e788d08d5ed004bac0e5b9a38
Es-say <eseobels@gmail.com>
2023-05-07 03:46:57 -0700

    prints numbers 0-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - Entry Point
  * Description -  prints all single digit numbers of base 10 starting from 0
  * Return: always 0
  */
 
-int main(void) 
+int main(void)
 {
 	int n;
 
@@ -14,6 +14,6 @@ int main(void)
 	{
 		printf("%d", n);
 	}
-	printf("\n");	
-	return(0);
+	printf("\n");
+	return (0);
 }
